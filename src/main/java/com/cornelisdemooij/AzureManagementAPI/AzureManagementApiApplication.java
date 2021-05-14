7fbb23d4bdc4c9e768428d68325484274b879607
Cornelis de Mooij <codemo@gmail.com>
2021-05-14 21:50:12 +0200

    First commit with basic API from the Spring Initializr.

---
+++ src/main/java/com/cornelisdemooij/AzureManagementAPI/AzureManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.cornelisdemooij.AzureManagementAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cornelisdemooij/AzureManagementAPI/AzureManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cornelisdemooij.AzureManagementAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
